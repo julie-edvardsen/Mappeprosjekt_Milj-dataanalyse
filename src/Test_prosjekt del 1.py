47e1948c7009d98a6961c9d636792c1a49bd9b16
julie_edvardsen <julie.holm.edvardsen@gmail.com>
2025-03-22 13:14:34 +0100

    Ny test

--- src/Test_prosjekt del 1.py
+++ src/Test_prosjekt del 1.py
@@ -1,4 +1,22 @@
 import kagglehub
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+import matplotlib.pylab as pylab
+params = {'legend.fontsize': 'x-large',
+          'figure.figsize': (15, 10),
+         'axes.labelsize': 'x-large',
+         'axes.titlesize':'x-large',
+         'xtick.labelsize':'x-large',
+         'ytick.labelsize':'x-large'}
+pylab.rcParams.update(params)
+
+plt.style.use('dark_background')
+
+df = pd.read_csv('../input/wind-power/data.csv')
+df['dt'] = pd.to_datetime(df['dt'])
 
 # Laster ned nyeste versjonen til datasettet fra Tyskland 
 pathGermany = kagglehub.dataset_download("l3llff/wind-power")
